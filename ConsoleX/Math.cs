362cc695c397fd2291fd6b3385a2948371e0d590
abhijitv <calljeet_2001@yahoo.com>
2019-05-14 12:32:11 +0800

    got delegate workikng

---
+++ ConsoleX/Math.cs
@@ -0,0 +1,42 @@
+﻿
+
+
+namespace ConsoleX
+{
+    public class Math
+    {
+
+        // declare a delegate 
+
+      
+        public delegate void PostTaskDelegate(double results);
+     
+        public PostTaskDelegate postDlg;
+
+        // initialize the delegate in constructor 
+
+
+        public Math()
+        {
+
+        }
+
+        public void Add(int x, int y)
+        {
+
+            double sum = x + y;
+            postDlg(sum);
+        }
+
+       public  void Multiply(int x, int y)
+
+        {
+            double product = x * y;
+            postDlg(product);
+
+        }
+
+     
+
+    }
+}
--- ConsoleX/Program.cs
+++ ConsoleX/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace ConsoleX
+
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the first  number");
+            string x = Console.ReadLine();
+            Console.WriteLine("enter the second number");
+            string y = Console.ReadLine();
+            Domath(Convert.ToInt32(x), Convert.ToInt32(12));
+
+        }
+
+      static   void Domath(int x, int y)
+        {
+
+            Math math = new Math();
+
+            math.postDlg = PublishResults;
+            math.Add(x, y);
+        
+
+        }
+
+        static  void PublishResults(double results)
+        {
+            Console.WriteLine(results);
+            Console.ReadLine();
+
         }
     }
 }
