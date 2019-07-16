774500d27c32f08a2364532e67869fc7e43b3c30
abhijitv <calljeet_2001@yahoo.com>
2019-07-16 15:44:04 +0800

    Totally new structure created

---
+++ ConsoleX/MathService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleX
+{
+    public class MathService
+    {
+
+        // Local variable for operation type 
+        OperationType opnType;
+
+        // variables for delegate only 
+        public delegate void MathComplete(double result); // delegate declaration 
+        public MathComplete MathCompleteDlg; // delegate that will be assigned the function at run time 
+
+
+
+        public void AddNumbersDelegateOnly(double x,  double y)
+        {
+
+            // do the addition 
+            double sum = x + y;
+
+            // Call the delegate 
+            MathCompleteDlg(sum);
+        }
+
+    }
+}
--- ConsoleX/Program.cs
+++ ConsoleX/Program.cs
@@ -7,62 +7,79 @@ using System.Net;
 namespace ConsoleX
 
 {
+
+    public enum OperationType
+    {
+
+        DelegateOnly,
+        EventOnly,
+        EventDelegate,
+        Action
+    }
+
     class Program
     {
-        static MathSvc4Delegate mathSvc4Delegate;
-     
-        static MathSvc4Event mathSvc4Event;
-        static MathSvc4Action mathSvc4Action;
-        
-         static void Main(string[] args)
+        static OperationType opnType;
+        static MathService MathSvc;
+
+       static  double x, y;
+        public static void Main()
+        {
+            // Get inputs from screen 
+            GetInputs(); // Get the inputs 
+
+            CallMathService();  // call the proper service 
+        }
+
+        public static void   GetInputs()
         {
 
-            Console.WriteLine("Please make your choice");
-            Console.WriteLine("1. Pure Delegates");
-            Console.WriteLine("2. Events");
-            Console.WriteLine("3. Actions");
-            Console.WriteLine("4. Lambda ");
-            Console.WriteLine("5. Async Download");
-
-            switch (Convert.ToInt32(Console.ReadLine()))
+            Console.WriteLine("Hello World! Please enter a number");
+            int result;
+            while( !int.TryParse(Console.ReadLine(),out result))
             {
-
-
-                case 1: // use only delegates 
-                    MathSvc MSvc = new MathSvc(OperationType.DelegateOnly);
-                    
-                    break;
-
-                case 2://using events instead of delegates 
-                    GetInputs();
-                    DoMath4Event(x, y);
-                    break;
-                case 3: // use actions instead of events and delegates 
-                    GetInputs();
-                    DoMath4Action(x, y);
-                    break;
-
-                case 4: // when the delegate class does not know anything about its being asked to do 
-                    GetInputs();
-                    mathSvc4Delegate = new MathSvc4Delegate();
-                    int result = 0;
-                    mathSvc4Delegate.DoSoneOperation += (x, y) =>
-                     {
-                         result = x + y;
-                         return result;
-                     };
-                    mathSvc4Delegate.DoSoneOperation(x, y);
-                    Console.WriteLine(result);
-                    Console.ReadLine();
-                    break;
-
-                case 5: // download some websites automatically 
-                    string path = "";
-                    DataPrep.BuildURLList(path);
-                    break;
-                      
-               
+                Console.WriteLine("Please enter a valid numerical value");
+                Console.WriteLine("Please enter a number");
+                
             }
+            
 
-           
+        }
+
+        public static  void CallMathService()
+        {
+            MathSvc = new MathService();
+
+            // assign the delegate  
+            MathSvc.MathCompleteDlg = PublishResults;
+
+            // call the right function  
+            switch (opnType)
+            {
+                case OperationType.DelegateOnly:
+                    MathSvc.AddNumbersDelegateOnly(x, y);
+                    break;
+                case OperationType.EventOnly:
+
+                    break;
+                case OperationType.EventDelegate:
+                    break;
+                case OperationType.Action:
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+
+
+     public   static void PublishResults(double result)
+        {
+            Console.WriteLine("The result of the operation is " + result);
+            GetInputs();
+        }
+
+    }
+
+
 }
